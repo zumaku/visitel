070c8bb53c169f23aee0a4819395fc0c7186ea0b
susilawatiii <susilawatiii256@gmail.com>
2024-02-22 09:50:07 +0800

    Menambah logo primary, secondary, dan logo mark

---
+++ resources/js/Components/logos/index.js
@@ -0,0 +1,9 @@
+import PrimaryLogo from "./PrimaryLogo";
+import SecondaryLogo from "./SecondaryLogo";
+import MarkLogo from "./MarkLogo";
+
+export {
+    PrimaryLogo,
+    SecondaryLogo,
+    MarkLogo,
+}
