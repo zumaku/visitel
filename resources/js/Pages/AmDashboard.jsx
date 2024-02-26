822219b41004765e1ce5d36f7b77b4d3fd793ef5
susilawatiii <susilawatiii256@gmail.com>
2024-02-26 16:05:08 +0800

    Menambah tombol laporan baru

--- resources/js/Pages/AmDashboard.jsx
+++ resources/js/Pages/AmDashboard.jsx
@@ -1,8 +1,10 @@
+import Buttons from "@/Components/Buttons";
 import AmLayout from "@/Layouts/AmLayout";
 import AmReportCard from "@/Components/AmReportCard";
 import AmCalender from "@/Components/AmCalender";
 import { Head } from "@inertiajs/react";
 
+import { DocumentPlusIcon } from "@heroicons/react/24/outline";
 export default function AmDashboard({ auth, semua_laporan }) {
     const laporan_terbaru = semua_laporan.slice(0, 3);
 
@@ -16,13 +18,21 @@ export default function AmDashboard({ auth, semua_laporan }) {
         >
             <Head title="Beranda" />
 
-            <h1 className={`text-h1`}>
-                <span className="text-primary">Hi,</span> {auth.user.name}
-            </h1>
-            <p className={`text-body mb-4`}>
-                Account Manager Divisi Bissunis Service at Witel Makassar,
-                Sul-Sel.
-            </p>
+            <div className="flex justify-between items-start">
+                <div className="">
+                    <h1 className={`text-h1`}>
+                        <span className="text-primary">Hi,</span> {auth.user.name}
+                    </h1>
+                    <p className={`text-body mb-4`}>
+                        Account Manager Divisi Bissunis Service at Witel Makassar,
+                        Sul-Sel.
+                    </p>
+                </div>
+                <Buttons className="ml-4" name="Laporan Baru" >
+                    <DocumentPlusIcon className="w-5 h-5" />
+                </Buttons>
+            </div>
+
             <div className="h-1 w-full bg-disable mb-4"></div>
 
             {/* Laporan Terbaru */}
