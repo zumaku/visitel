58efe082806f4a61d1ab137e8a98df2e2d2632f6
zumaku <60200121101@uin-alauddin.ac.id>
2024-02-28 09:49:54 +0800

    Jodit Editor

--- resources/js/Pages/Playground.jsx
+++ resources/js/Pages/Playground.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
+import JoditEditor from "jodit-react";
 
-const Playground = () => {
-    return (
-        <></>
-    );
+const Editor = ({ placeholder }) => {
+	const [content, setContent] = useState('');
+
+	const config = {
+		readonly: false,
+		placeholder: placeholder || 'Start typings...'
+	};
+
+	const handleBlur = (newContent) => {
+		setContent(newContent);
+	};
+
+    const handleKirim = () => {
+        console.log(content);
+    }
+
+	return (
+		<>
+            <JoditEditor
+                value={content}
+                config={config}
+                tabIndex={1} // tabIndex of textarea
+                onBlur={handleBlur} // preferred to use only this option to update the content for performance reasons
+            />
+            <button onClick={handleKirim}>Kirim</button>
+        </>
+	);
 };
 
-export default Playground;
+export default function Playground() {
+    return (
+        <div className="w-full flex-center p-10">
+            <Editor placeholder={"Editorku"} />
+        </div>
+    )
+}
